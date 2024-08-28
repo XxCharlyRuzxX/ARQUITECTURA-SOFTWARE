22de535761339d7e9e494cfc9fba7c2953117ef9
RafaMadera18 <rafa_madlop@hotmail.com>
2024-08-28 17:36:45 -0600

    Arreglo CSVReader Elimine Comentarios

--- ServicePdfGenerator/src/main/java/com/mycompany/servicepdfgenerator/CsvScanner/CsvLineFormatValidator.java
+++ ServicePdfGenerator/src/main/java/com/mycompany/servicepdfgenerator/CsvScanner/CsvLineFormatValidator.java
@@ -3,38 +3,8 @@ package com.mycompany.servicepdfgenerator.CsvScanner;
 
 public class CsvLineFormatValidator {
 
-    private int adresserIndex;
-    private int senderIndex;
-
     public CsvLineFormatValidator(){
-        this.adresserIndex = -1;
-        this.senderIndex = -1;
     }
-
-
-    public int getAdresserIndex(){
-        return this.adresserIndex;
-    }
-
-    public int getSenderIndex(){
-        return this.senderIndex;
-    }
-
-    /*
-    public void checkCsvIdentifiers(String[] identifiers) throws IllegalArgumentException {
-        for(int i = 0; i < identifiers.length; i++){
-            if (identifiers[i].trim().equalsIgnoreCase("destinatario")){
-                this.adresserIndex = i;
-            } else if (identifiers[i].trim().equalsIgnoreCase("remitente")) {
-                this.senderIndex = i;
-            }
-        }
-
-        if (this.adresserIndex == -1 | this.senderIndex == -1){
-            throw new IllegalArgumentException("Las palabras claves destinatario o remitente no fueron declaradas en el CSV");
-        }
-
-    }*/
 
     public void checkCsvKeys(String[] keys) throws IllegalArgumentException {
         if(keys.length < 1){
